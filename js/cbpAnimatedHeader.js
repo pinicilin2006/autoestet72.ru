5e3916e155dfc6f23fdd83316c63175dcbb5e643
husaniov_aa <husaniov_aa@sngi.ru>
2015-11-30 15:48:03 +0500

    Осталось схема проезда и прайсы

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -30,7 +30,7 @@ var cbpAnimatedHeader = (function() {
 			classie.add( header, 'navbar-shrink' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			classie.add( header, 'navbar-shrink' );
 		}
 		didScroll = false;
 	}
